1a5a858e3637e144b178751bdafcd6a543a56c8a
maaqoul <hicham.maaqoul@gmail.com>
2020-07-30 23:30:18 +0100

    add build file

---
+++ build/precache-manifest.2cad816f9c06d80f8d682810fd4000bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359e96b19b1c11d0497383b1816b430e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18bb3601edd16e20ed4c",
+    "url": "/static/css/main.587049cd.chunk.css"
+  },
+  {
+    "revision": "5c6036a46d77edb2546b",
+    "url": "/static/js/2.3f02f833.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3f02f833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18bb3601edd16e20ed4c",
+    "url": "/static/js/main.1f982c72.chunk.js"
+  },
+  {
+    "revision": "45dc7ea1fbf55456baa5",
+    "url": "/static/js/runtime-main.f109c77a.js"
+  }
+]);
---
+++ build/precache-manifest.348fa04aac2eef2a5be5c643db637809.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b05de04aa5f75e11c705de993c63307",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d91b5ba8973009e2e04a",
+    "url": "/static/css/main.587049cd.chunk.css"
+  },
+  {
+    "revision": "5c6036a46d77edb2546b",
+    "url": "/static/js/2.3f02f833.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3f02f833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91b5ba8973009e2e04a",
+    "url": "/static/js/main.2acb44da.chunk.js"
+  },
+  {
+    "revision": "45dc7ea1fbf55456baa5",
+    "url": "/static/js/runtime-main.f109c77a.js"
+  }
+]);
