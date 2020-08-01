991b1037a18dd0e105f139f84dbe59425ab3fe7a
maaqoul <hicham.maaqoul@gmail.com>
2020-08-02 00:12:00 +0100

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    }
+}
